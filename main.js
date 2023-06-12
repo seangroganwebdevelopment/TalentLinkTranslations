09993513b0de0ee030c8b35e8fbb816aef28a3d6
Sean Grogan <seangrogan11@gmail.com>
2023-06-12 17:53:39 +0200

    6pm

---
+++ main.js
@@ -0,0 +1,9 @@
+const colorButton = document.getElementById('colorButton');
+const body = document.body;
+
+colorButton.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
+
+
+
